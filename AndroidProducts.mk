7f5825c579c77f89f94a99eb8975a559d66c906d
anhdat1024 <tadat1024@gmail.com>
2025-07-22 10:34:35 +0700

    a52sxq: InfinityX bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_a52sxq.mk
+    $(LOCAL_DIR)/infinity_a52sxq.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_a52sxq-userdebug \
-    lineage_a52sxq-user \
-    lineage_a52sxq-eng
+    infinity_a52sxq-userdebug \
+    infinity_a52sxq-user \
+    infinity_a52sxq-eng
