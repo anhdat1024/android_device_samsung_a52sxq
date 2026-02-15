77ba0d7074b053c774b7dbce139552452082a064
anhdat1024 <tadat1024@gmail.com>
2026-02-15 10:47:17 +0700

    a52sxq: Introduce GameBar
    Change-Id: I68e0fdedf47a33e6bbbb2c15eba6e1ece1f47129

--- device.mk
+++ device.mk
@@ -35,6 +35,9 @@ PRODUCT_COPY_FILES += \
 # BCR
 $(call inherit-product-if-exists, vendor/bcr/bcr.mk)
 
+# GameBar
+$(call inherit-product-if-exists, packages/apps/GameBar/gamebar.mk)
+
 # Sensors
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
